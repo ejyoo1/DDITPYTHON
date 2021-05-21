bb8d2cbca55c08766bd94447d17f5e376dc8aa14
ejyoo1 <o3k3585@gmail.com>
2021-05-21 21:48:01 +0900

    이미지 띄우기

--- HTLLOWPYTHON/day12/mycv02.py
+++ HTLLOWPYTHON/day12/mycv02.py
@@ -1,8 +1,10 @@
 import cv2
 # RGB 는 openCV에서 BGR로 읽고 nump로 처리한다.(빠르게 연산하기 위해서)
 # 이미지 읽기
-img = cv2.imread('1.png', 1)
-print(img)
+img_src = cv2.imread('1.png', 1)
+
+# 흑백 명암(그레이스케일)으로 이미지를 변환
+img = cv2.cvtColor(img_src, cv2.COLOR_BGR2GRAY) 
  
 # 이미지 화면에 표시
 cv2.imshow('Test Image', img)
